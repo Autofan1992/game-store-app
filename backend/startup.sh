25a8de1f5c24232a97a69da628f0e4c6d8aba91f
Mykola <64962311+Autofan1992@users.noreply.github.com>
2024-04-20 14:14:06 +0300

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-command: node -e 'console.log(process.env.DATABASE_URL);'
-
-
-yarn db:generate && yarn db:migrate && yarn db:seed && yarn build
-node dist/index.js
+yarn db:generate && yarn db:migrate && yarn db:seed && db:studio && yarn build && node dist/index.js
