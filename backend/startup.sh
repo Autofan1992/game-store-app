8c7c3da605395bb5632f21431040546e13b2bce4
Mykola <64962311+Autofan1992@users.noreply.github.com>
2024-04-20 14:28:24 +0300

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yarn db:generate && yarn db:migrate && yarn db:seed && db:studio && yarn build && node dist/index.js
+yarn db:generate && yarn db:migrate && yarn db:seed && yarn db:studio && yarn build && node dist/index.js
